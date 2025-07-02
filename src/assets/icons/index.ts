8af8bfd56df4532a4eab1e68a9e79016ac7680f4
alex blade <aalexblade@gmail.com>
2025-07-02 23:06:27 +0300

    add impor img in index.ts

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -12,3 +12,32 @@ import mobile2 from './hero/mobile-img2.jpg';
 import mobile3 from './hero/mobile-img3.jpg';
 
 export { desktop1, desktop2, desktop3, tablet1, tablet2, tablet3, mobile1, mobile2, mobile3 };
+
+// images Section 3 Dress with love
+import section3desktop1 from './imagesSection3/section3desktop1.jpg';
+import section3desktop2 from './imagesSection3/section3Desktop2.jpg';
+import section3desktopCarusel1 from './imagesSection3/section3-desktopCarusel1.jpg';
+import section3desktopCarusel2 from './imagesSection3/section3-desktopCarusel2.jpg';
+import section3desktopCarusel3 from './imagesSection3/section3-desktopCarusel3.jpg';
+
+import section3tablet1 from './imagesSection3/section3tablet1.jpg';
+import section3tablet2 from './imagesSection3/section3tablet2.jpg';
+import section3tablet3 from './imagesSection3/section3tablet3.jpg';
+
+import section3mobile1 from './imagesSection3/section3mobile1.jpg';
+import section3mobile2 from './imagesSection3/section3mobile2.jpg';
+import section3mobile3 from './imagesSection3/section3mobile1.jpg';
+
+export {
+  section3desktop1,
+  section3desktop2,
+  section3desktopCarusel1,
+  section3desktopCarusel2,
+  section3desktopCarusel3,
+  section3tablet1,
+  section3tablet2,
+  section3tablet3,
+  section3mobile1,
+  section3mobile2,
+  section3mobile3,
+};
