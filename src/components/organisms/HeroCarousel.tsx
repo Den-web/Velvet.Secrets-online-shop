e71543f81683a85d18f102cd1e0235e21e2e244e
alex blade <aalexblade@gmail.com>
2025-07-02 13:42:02 +0300

    add index.js for impor img in images

--- src/components/organisms/HeroCarousel.tsx
+++ src/components/organisms/HeroCarousel.tsx
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import { Carousel } from 'antd';
-// import HeroSlide from '../molecules/HeroSlider';
-// import { useSelector } from 'react-redux';
-// import type { RootState } from '../../store';
-// import { heroSlides } from '../../data/heroSlides';
-
-// const HeroCarousel: React.FC = () => {
-//   const width = useSelector((state: RootState) => state.window.width);
-//   const screen: 'desktop' | 'tablet' | 'mobile' =
-//     width < 768 ? 'mobile' : width < 1024 ? 'tablet' : 'desktop';
-
-//   return (
-//     <Carousel autoplay dots className="relative z-10">
-//       {heroSlides.map((slide, index) => (
-//         <HeroSlide
-//           key={index}
-//           title={slide.title}
-//           subtitle={slide.subtitle}
-//           image={slide[screen]}
-//         />
-//       ))}
-//     </Carousel>
-//   );
-// };
-
-// export default HeroCarousel;
-
 import React from 'react';
 import { Carousel } from 'antd';
 import HeroSlide from '../molecules/HeroSlider';
--- src/data/heroSlides.ts
+++ src/data/heroSlides.ts
@@ -1,14 +1,14 @@
-import desktop1 from '../assets/icons/hero/desktop-img1.jpg';
-import desktop2 from '../assets/icons/hero/desktop-img2.jpg';
-import desktop3 from '../assets/icons/hero/desktop-img3.jpg';
-
-import tablet1 from '../assets/icons/hero/tablet-img1.jpg';
-import tablet2 from '../assets/icons/hero/tablet-img2.jpg';
-import tablet3 from '../assets/icons/hero/tablet-img3.jpg';
-
-import mobile1 from '../assets/icons/hero/mobile-img1.jpg';
-import mobile2 from '../assets/icons/hero/mobile-img2.jpg';
-import mobile3 from '../assets/icons/hero/mobile-img3.jpg';
+import {
+  desktop1,
+  desktop2,
+  desktop3,
+  tablet1,
+  tablet2,
+  tablet3,
+  mobile1,
+  mobile2,
+  mobile3,
+} from '../assets/icons/index';
 
 export const heroSlides = [
   {
